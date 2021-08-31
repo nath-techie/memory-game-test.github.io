0d24c0cfe725853b2d02576635724185f01002c1
NathanSabari <64378408+nath-techie@users.noreply.github.com>
2021-09-01 03:04:22 +0530

    Update simcode.js

--- simcode.js
+++ simcode.js
@@ -1,16 +1,19 @@
+
+
 var butc=["red","blue","green","yellow"];
 var pat=[];
 var upat=[];
 var start=0;
-var level=0;var unk=0;
+var level=0;var unwantedclik=0;var sound=0;
+var gs=new Audio ('gameover.mp3');
 $("#level").on("click",function()
 { if(start!=1){
-   sequence();unk=1;
+   sequence();unwantedclik=1;
  }start=1;
 });
 
 $(".button").click(function userclick()
-{if(unk==1){
+{if(unwantedclik==1){
   var clickedbtn=$(this).attr("id");
   animate(clickedbtn);
 upat.push(clickedbtn);console.log(upat);
@@ -21,7 +24,7 @@ else{ $("#level").prepend("<span>👉</span>");
 });
 
 function sequence(){upat=[];
-  level++;console.log(level);
+  level++;console.log(level);sounds();
  $("#level").text("Level "+level);
 var ran=Math.floor(Math.random()*4);
 var rbutc=butc[ran];
@@ -30,12 +33,18 @@ pat.push(rbutc);console.log(pat);animate(rbutc);
 
 function checkanswer(a)
 {if(pat[a]===upat[a])
-  {if (pat.length==upat.length) {console.log("succes");setTimeout(function(){sequence();},1000); }
+  {if (pat.length==upat.length)
+    {console.log("succes");
+    setTimeout(function(){sequence();},1000); }
   }
   else {
     $("#level").text("Game Over");
-    $("#ref").text(" Refresh the page to Restart");
+    $("#ref").text(" Refresh the page to Restart");gs.play();
   }
 }
 
+$("#mute").click(function m(){sound=1;$(this).text("🔇");});
 function animate(a){$("#"+a).fadeIn(100).fadeOut(100).fadeIn(100);}
+function sounds(){if (sound==0) {
+  var s=new Audio("c.mp3");s.play();
+}}
